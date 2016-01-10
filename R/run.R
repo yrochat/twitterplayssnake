c517ae694504d60521e7da8f00fabeb843e18a63
yrochat <yannick.rochat@gmail.com>
2016-01-10 22:21:46 +0100

    tweets retrieval

--- R/run.R
+++ R/run.R
@@ -1,1 +1,81 @@
-display <- init_board()
+# 0. Twitter authentication
+# 1. Load last state
+# 2. Is it a new game ?
+# 2a. If yes, initialise a game, tweet it and BREAK.
+# 2b. If no, get tweets from the last 5 minutes. 
+# 2ba. If none, KEEP SAME DIRECTION.
+# 2bb. If yes, Extract direction and username from last tweet.
+# 3a. If new, update usernames count.
+# 3b. If an old tweet, drop username.
+# 4. 
+
+
+
+
+source("my_tokens.R")
+
+# callback url http://127.0.0.1:1410
+
+options(httr_oauth_cache=TRUE) 
+setup_twitter_oauth(api_key, api_secret, access_token, access_token_secret)
+
+# run only the first time
+#
+# display <- init_board()
+# tweet(draw_board(display))
+# save(display, file = "display.Rdata")
+#
+
+# Load last state
+
+load("display.Rdata")
+
+# If the game was finished, start a new one and stop the script
+
+if (display$finished == T) {
+  display <- init_board()
+  tweet(draw_board(display))
+  save(display, file = "display.Rdata")
+  stop("Game initialised")
+}
+
+# If the game was not finished, get the last tweets
+
+load("last_mention_id.Rdata")
+
+last_mentions <- twListToDF(mentions())
+last_mentions <- last_mentions[last_mentions$id > id_threshold,]
+
+# We update the value for the last ID
+
+id_threshold <- last_mentions$id[1]
+if (length(id_threshold) > 0) {
+  save(id_threshold, file = "last_mention_id.Rdata")
+}
+
+# We select the tweets with values for direction
+
+ok_mentions <- last_mentions[grepl(directions.collapsed, last_mentions$text),]
+
+if (nrow(ok_mentions) > 0) {
+  regex <- regexpr(directions.collapsed, ok_mentions$text)
+  regex <- match(regmatches(ok_mentions$text, regex), directions.df$directions)
+  if (length(regex) == 1) {
+  	display$direction <- as.numeric(regex)
+  } else {
+  	display$direction <- as.numeric(sample(directions.df$code[regex], 1))
+  }
+  update_board(display, new_direction = display$direction)
+} else {
+  update_board(display, new_direction = display$direction)	
+}
+
+
+
+
+
+
+
+
+
+
--- R/snake.R
+++ R/snake.R
@@ -1,49 +1,77 @@
-library(plyr)
+rm(list=ls())
+
+library(httr)
+library(twitteR)
+
+is_on_the_border <- function(pos1, pos2, height, width) {
+  pos1 == pos2 | any(((pos1 - 1) %/% width + 1) %in% c(1, height)) | any(((pos1) %% width) %in% c(0, 1))
+}
 
 init_board <- function(height = 5, width = 10) {
-	
+
   # empty board
-  display <- list(board = matrix(),
-                  mouse = numeric(),
-                  snake = vector(),
-                  direction = numeric())
-                  
-  display$mouse <- sample(width*height, 1)
-  display$snake <- sample(width*height, 1)
-  
-  while (display$snake == display$mouse | any((display$snake %% height + 1) %in% c(1, height)) | any((display$snake %% width) %in% c(1, width))) {
-    display$snake <- sample(width*height, 1)
+  display <- list(board = matrix(), mouse = numeric(), snake = vector(), direction = numeric(), finished = logical())
+
+  display$mouse <- sample(width * height, 1)
+  display$snake <- sample(width * height, 1)  
+
+  while (is_on_the_border(display$snake, display$mouse, height = height, width = width)) {
+    display$snake <- sample(width * height, 1)
   }
-  
+
   display$direction <- sample(4, 1)
   display$board <- matrix(data = 0, nrow = height, ncol = width)
   display$board[display$mouse] <- 5
-  display$board[display$snake] <- display$direction
+  display$board[display$snake] <- 6
   
+  display$finished <- F
+
   return(display)
 }
 
-# 0 empty square \xE2\xAC\x9C
-# 1 right arrow  \xE2\x9E\xA1
-# 2 up    arrow  \xE2\xAC\x86
-# 3 left  arrow  \xE2\xAC\x85
-# 4 down  arrow  \xE2\xAC\x87
-# 5 mouse        \xF0\x9F\x90\xAD
+# 0 empty square \u2b1c
+# 1 right arrow  ➡
+# 2 up    arrow  \u2b06
+# 3 left  arrow  \u2b05
+# 4 down  arrow  \u2b07
+# 5 mouse        \U0001f42d
+# 6 snake        \xF0\x9F\x90\x8D
 
-emoji.table <- data.frame(code = 0:5, 
-                          emoji = c("\xE2\xAC\x9C", "\xE2\x9E\xA1", "\xE2\xAC\x86", "\xE2\xAC\x85", "\xE2\xAC\x87", "\xF0\x9F\x90\xAD"), 
-                          stringsAsFactors = F)
+emoji.table <- data.frame(code = 0:6, emoji = c("\u2b1c", "➡", "\u2b06", "\u2b05", "\u2b07", "\U0001f42d", "\xF0\x9F\x90\x8D"), stringsAsFactors = F)
 
 draw_board <- function(display) {
-  tweet <- lapply(1:nrow(display$board), function(n) emoji.table$emoji[match(display$board[n,], emoji.table$code)])
+  tweet <- lapply(1:nrow(display$board), function(n) emoji.table$emoji[match(display$board[n, ], emoji.table$code)])
   tweet <- lapply(tweet, function(x) paste0(x, collapse = ""))
-  tweet <- do.call(rbind, tweet)[,1]
+  tweet <- do.call(rbind, tweet)[, 1]
   tweet <- paste0(tweet, collapse = "\n")
   return(tweet)
 }
 
+directions <- c("droite", "right", "rightwards", "➡",
+                "haut", "top", "upwards", "\u2b06",
+                "gauche", "left", "leftwards", "\u2b05",
+                "bas", "bottom", "downwards", "\u2b07")
+                
+directions.df <- data.frame(directions = directions, code = as.vector(sapply(1:4, function(n) rep(n, 4))), stringsAsFactors = F)
+
+directions.collapsed <- paste0(directions.df$directions, collapse = "|")
+
+update_board <- function(display = display, new_direction = display$direction) {
+  
+}
 
 
-# display <- init_board()
-# hey <- draw_board(display)
-# tweet(hey)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
